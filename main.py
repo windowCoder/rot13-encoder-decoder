bde5da6de13eb9f588841126e991e4b9be846a15
Matthew <40544801+windowCoder@users.noreply.github.com>
2021-04-03 00:05:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 # 1. text <= 13 (a-m) can have 13 added to get the cipher (n-z)
 # 2. text >= 12 (n-z) can have 13 subtracted to get the cipher (a-m)
 alphabet, x, the_message = 'abcdefghijklmnopqrstuvwxyz', 0, ''
-alphabetBoth, alphabetLow, alphabetHigh, msg = [], [], [], []
+alphabetBoth, alphaLower, alphaUpper, msg = [], [], [], []
 
 for i in alphabet:  # CREATE 3 alphabet lists; lowercase, uppercase, and both combined.
-    alphabetLow.append(alphabet[x])
-    alphabetHigh.append(alphabet[x].upper())
+    alphaLower.append(alphabet[x])
+    alphaUpper.append(alphabet[x].upper())
     alphabetBoth.append(alphabet[x].upper())
     alphabetBoth.append(alphabet[x].lower())
     x += 1
@@ -15,18 +15,18 @@ for i in alphabet:  # CREATE 3 alphabet lists; lowercase, uppercase, and both co
 entry = input("[92mrot13 encode/decode \nentry: [0m")
 
 for string in entry:    #  COMPARE each string and how it relates to the lists.
-    if string not in alphabetBoth:  # non alphabet
+    if string in alphaLower:  # if string is LOWER case
+        if alphaLower.index(string) >= 13:  # and high
+            msg.append(alphaLower[alphaLower.index(string) - 13])
+        else:
+            msg.append(alphaLower[alphaLower.index(string) + 13])
+    elif string in alphaUpper:  # if string is UPPER case
+        if alphaUpper.index(string) >= 13:  # and high
+            msg.append(alphaUpper[alphaUpper.index(string) - 13])
+        else:
+            msg.append(alphaUpper[alphaUpper.index(string) + 13])
+    else:
         msg.append(string)
-    elif string in alphabetLow:  # if string is LOWER case
-        if alphabetLow.index(string) >= 13:  # and high
-            msg.append(alphabetLow[alphabetLow.index(string) - 13])
-        if alphabetLow.index(string) <= 12:  # or low
-            msg.append(alphabetLow[alphabetLow.index(string) + 13])
-    elif string in alphabetHigh:  # if string is UPPER case
-        if alphabetHigh.index(string) >= 13:  # and high
-            msg.append(alphabetHigh[alphabetHigh.index(string) - 13])
-        if alphabetHigh.index(string) <= 12:  # or low
-            msg.append(alphabetHigh[alphabetHigh.index(string) + 13])
 
 for k in msg:   # COMBINE list msg to form one string
     the_message += k
